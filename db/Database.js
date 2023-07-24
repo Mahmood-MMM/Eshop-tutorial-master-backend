5e479ab724b4cd843961fb49dedfe4989bfe62d9
Mahmood Osman <mahmoodosman797@gmail.com>
2023-07-24 22:15:46 +0300

    changes

--- db/Database.js
+++ db/Database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.DB_URL, {
+    .connect("mongodb+srv://mahmoodna3n3:cstHO7bhqMkisEUr@cluster0.xdpgzxj.mongodb.net/?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
